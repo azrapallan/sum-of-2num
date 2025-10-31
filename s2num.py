7eb0ae82ef791985cbecf257ad1bb758c3b51a29
azrapallan <pallanazra@gmail.com>
2025-10-31 09:51:05 +0530

    Update s2num.py

--- s2num.py
+++ s2num.py
@@ -1,6 +1,6 @@
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = 10
+num2 = 20
 sum = num1 + num2
 
 print("The sum of", num1, "and", num2, "is:", sum)
